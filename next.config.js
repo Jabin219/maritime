201d3f6a52b6f2902d07b507bb60e87ff7156c11
Jiabin Liu <jiabin@zmley.com>
2022-03-01 10:18:29 -0400

    delete env from next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // env: {
-  //   mongodburl: "mongodb+srv://maritime:zmleydev123@maritime.hkhwb.mongodb.net/dev?retryWrites=true&w=majority",
-  // }
 }
 
 module.exports = nextConfig
