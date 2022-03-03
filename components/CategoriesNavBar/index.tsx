b4fde94014827debeeea630990621cf1da7df671
Jiabin Liu <jiabin@zmley.com>
2022-03-03 13:11:20 -0400

    add router

--- components/CategoriesNavBar/index.tsx
+++ components/CategoriesNavBar/index.tsx
@@ -3,8 +3,10 @@ import { CustomTab } from './style'
 import { Categories } from './constant'
 import { ProductContext } from '../../context/ProductContextProvider'
 import { useContext } from 'react'
+import { useRouter } from 'next/router'
 
 function CategoriesNavBar() {
+	const router = useRouter()
 	const { setCategory } = useContext(ProductContext)
 	return (
 		<Box sx={{ boxShadow: '0px 2px 10px rgba(0, 0, 0, 0.1)' }}>
@@ -16,6 +18,7 @@ function CategoriesNavBar() {
 						label={item.label}
 						onClick={() => {
 							setCategory(item.value)
+							router.push('/product-list')
 						}}
 						sx={
 							item.label === 'SALE'
