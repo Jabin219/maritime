bfd182c91906be421bec964e4c0c1c554e73cc17
Jiabin Liu <jiabin@zmley.com>
2022-03-09 17:02:48 -0400

    add components

---
+++ components/header/index.tsx
@@ -0,0 +1,90 @@
+import {
+	AppBar,
+	Box,
+	Grid,
+	InputAdornment,
+	Tabs,
+	TextField
+} from '@mui/material'
+import Image from 'next/image'
+import { CustomTab, CustomGrid, HeaderButton } from './style'
+import { Search, ShoppingCart } from '@mui/icons-material'
+import CategoriesNavBar from '../categoriesNavBar'
+import { HeaderLinks } from './constant'
+import CustomLink from '../customLink'
+
+function Header() {
+	return (
+		<AppBar position='static' color='secondary' sx={{ boxShadow: 'none' }}>
+			<Grid
+				container
+				sx={{ margin: '13px 13px 0', borderBottom: '1px solid #ADADAD' }}
+			>
+				<Grid item xs>
+					<CustomLink href='/'>
+						<Image
+							src='/images/logo/header-logo.png'
+							alt='logo'
+							width={155}
+							height={95}
+						/>
+					</CustomLink>
+				</Grid>
+				<CustomGrid item sx={{ display: 'flex', alignItems: 'center' }} xs={4}>
+					<Box>
+						<Tabs className='nav' value={false} indicatorColor='secondary'>
+							{HeaderLinks.map((item, index) => (
+								<CustomLink key={index} href={item.link}>
+									<CustomTab
+										key={index}
+										value={item.value}
+										label={item.label}
+									></CustomTab>
+								</CustomLink>
+							))}
+						</Tabs>
+					</Box>
+				</CustomGrid>
+				<CustomGrid item xs={3}>
+					<TextField
+						fullWidth
+						size='small'
+						InputProps={{
+							startAdornment: (
+								<InputAdornment position='start'>
+									<Search />
+								</InputAdornment>
+							)
+						}}
+					></TextField>
+				</CustomGrid>
+				<CustomGrid item xs={1}>
+					<CustomLink href='/order'>
+						<ShoppingCart />
+					</CustomLink>
+				</CustomGrid>
+				<CustomGrid item xs={3} sx={{ marginRight: '1vw' }}>
+					<CustomLink href='/login'>
+						<HeaderButton
+							sx={{
+								backgroundColor: '#EEEEEE',
+								color: '#333333',
+								marginRight: '2vw'
+							}}
+						>
+							Log in
+						</HeaderButton>
+					</CustomLink>
+					<CustomLink href='/sign-up'>
+						<HeaderButton sx={{ backgroundColor: '#FF8800', color: '#ffffff' }}>
+							Sign up
+						</HeaderButton>
+					</CustomLink>
+				</CustomGrid>
+			</Grid>
+			<CategoriesNavBar />
+		</AppBar>
+	)
+}
+
+export default Header
---
+++ components/pageBanner/index.tsx
@@ -0,0 +1,42 @@
+import { Typography } from '@mui/material'
+import { useState, useEffect } from 'react'
+import { getBannerHeight } from 'utils'
+import { FlexBox } from '../customComponents'
+
+interface Props {
+	pageTitle: string
+	bannerUrl: string
+	pageDescription: string
+}
+
+function PageBanner({ pageTitle, pageDescription, bannerUrl }: Props) {
+	const [bannerHeight, setBannerHeight] = useState(400)
+	useEffect(() => {
+		setBannerHeight(getBannerHeight(3.6))
+	}, [])
+	return (
+		<FlexBox
+			className='page-banner-container'
+			sx={{
+				width: '100%',
+				height: bannerHeight,
+				background: `url(${bannerUrl}) no-repeat`,
+				backgroundSize: 'cover',
+				backgroundPosition: 'center'
+			}}
+		>
+			{pageTitle && (
+				<Typography variant='h1' sx={{ color: '#ffffff' }}>
+					{pageTitle}
+				</Typography>
+			)}
+			{pageDescription && (
+				<Typography variant='h6' sx={{ color: '#ffffff' }}>
+					{pageDescription}
+				</Typography>
+			)}
+		</FlexBox>
+	)
+}
+
+export default PageBanner
