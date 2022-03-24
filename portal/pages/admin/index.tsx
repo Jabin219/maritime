759ca0b58dd629e04a869a6762da99384da99deb
Jiabin Liu <jiabin@zmley.com>
2022-03-24 09:56:37 -0300

    admin menu update

--- portal/pages/admin/index.tsx
+++ portal/pages/admin/index.tsx
@@ -1,4 +1,5 @@
 import { Box, Grid } from '@mui/material'
+import AdminMenu from 'components/admin/adminMenu'
 import AdminLogin from './login'
 import AdminOrders from './orders'
 const AdminHome = () => {
@@ -8,8 +9,10 @@ const AdminHome = () => {
 		} else {
 			return (
 				<Grid container>
-					<Grid item></Grid>
-					<Grid item>
+					<Grid item xs={0.6}>
+						<AdminMenu />
+					</Grid>
+					<Grid item xs>
 						<AdminOrders />
 					</Grid>
 				</Grid>
