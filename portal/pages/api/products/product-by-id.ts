8a7b7da4623b84b3a1d88db88009a9cfb484218e
Jiabin Liu <jiabin@zmley.com>
2022-04-27 12:01:17 -0300

    add recommended products function

--- portal/pages/api/products/product-by-id.ts
+++ portal/pages/api/products/product-by-id.ts
@@ -1,12 +1,27 @@
 import connectDB from '../middleware/mongodb'
-import Product from 'models/mongodb/product'
+import ProductModel from 'models/mongodb/product'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { ResponseStatus } from 'constant'
+import * as mongodb from 'mongodb'
 
+const { ObjectId } = mongodb
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 	const { productId } = req.query
 	try {
-		const product = await Product.findOne({ _id: productId })
+		const product = await ProductModel.findOne({ _id: productId })
+		const recommendedProducts = await ProductModel.aggregate([
+			{
+				$match: {
+					$and: [
+						{ _id: { $ne: new ObjectId(String(productId)) } },
+						{ category: String(product._doc.category) },
+						{ stock: { $gte: 1 } }
+					]
+				}
+			},
+			{ $sample: { size: 4 } }
+		])
+		product._doc.recommendedProducts = recommendedProducts
 		if (!product) {
 			res.status(200).json({
 				status: ResponseStatus.NOT_FOUND,
