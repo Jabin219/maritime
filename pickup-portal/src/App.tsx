d6c405808ba658fd41f0eafcdf7981a9a0e0a309
Jiabin Liu <jiabin@zmley.com>
2022-05-04 15:13:42 -0300

    footer style update

--- pickup-portal/src/App.tsx
+++ pickup-portal/src/App.tsx
@@ -4,13 +4,16 @@ import TextContextProvider from 'contexts/TextContext'
 import Header from 'components/Header'
 import OrderContextProvider from 'contexts/OrderContext'
 import AppRoutes from 'routes'
+import { Box } from '@mui/material'
 
 function App() {
 	return (
 		<OrderContextProvider>
 			<TextContextProvider>
 				<Header />
-				<AppRoutes />
+				<Box sx={{ paddingBottom: '100px' }}>
+					<AppRoutes />
+				</Box>
 				<FooterTabs />
 			</TextContextProvider>
 		</OrderContextProvider>
