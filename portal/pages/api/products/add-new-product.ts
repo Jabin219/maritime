567abac3caf10f867476448070c29132d75d03ec
Jiabin Liu <jiabin@zmley.com>
2022-05-09 23:44:45 -0300

    Update add-new-product.ts

--- portal/pages/api/products/add-new-product.ts
+++ portal/pages/api/products/add-new-product.ts
@@ -18,16 +18,11 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 	})
 	const s3 = new AWS.S3()
 	const imageUrls: string[] = []
-	imageBase64Urls.forEach(async (imageBase64Url: string) => {
+	for await (const imageBase64Url of imageBase64Urls) {
 		const base64Data = Buffer.from(
 			imageBase64Url.replace(/^data:image\/\w+;base64,/, ''),
 			'base64'
 		)
-		const data = {
-			Key: uuidv4(),
-			Body: imageBase64Url,
-			ContentEncoding: 'base64'
-		}
 		// Getting the file type, ie: jpeg, png or gif
 		const fileType = imageBase64Url.split(';')[0].split('/')[1]
 		const params = {
@@ -38,27 +33,27 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 			ContentEncoding: 'base64', // required
 			ContentType: `image/${fileType}` // required. Notice the back ticks
 		}
-
 		// The upload() is used instead of putObject() as we'd need the location url and assign that to our user profile/database
 		// see: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#upload-property
 		let location = ''
 		let key = ''
 		try {
-			const { Location, Key } = await s3.upload(params).promise()
-			location = Location
-			key = Key
+			const result = await s3.upload(params).promise()
+			location = result.Location
+			key = result.Key
+			imageUrls.push(location)
 		} catch (error) {
 			console.error(error)
 		}
-		imageUrls.push(location)
-	})
+	}
+
 	try {
 		const product = new ProductModel({
 			name,
 			price,
 			originalPrice,
 			coverImage: imageUrls[0],
-			images: imageUrls,
+			images: JSON.stringify(imageUrls),
 			category,
 			stock
 		})
