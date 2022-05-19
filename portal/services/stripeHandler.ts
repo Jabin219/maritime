cc2b6f92e5e84674c6c80ee0044ed83c49e8a3f6
Jiabin Liu <jiabin@zmley.com>
2022-05-19 11:53:03 -0300

    stripe webhook function update

--- portal/services/stripeHandler.ts
+++ portal/services/stripeHandler.ts
@@ -4,15 +4,11 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
 	apiVersion: '2020-08-27'
 })
 
-const createPaymentIntent = (
-	totalPrice: number | string,
-	orderId: string,
-	orderedProducts: string
-) =>
+const createPaymentIntent = (totalPrice: number | string, orderId: string) =>
 	stripe.paymentIntents.create({
 		amount: Number((Number(totalPrice) * 100).toFixed(0)),
 		currency: 'cad',
-		metadata: { orderId, orderedProducts } || {}
+		metadata: { orderId } || {}
 	})
 
 export { createPaymentIntent, stripe }
